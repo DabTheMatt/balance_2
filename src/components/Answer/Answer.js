f60ef7c4b63ed7e79d377f32132903c15ae758f6
DabTheMatt <dabrowskimaciej@protonmail.com>
2022-09-20 20:45:33 +0200

    hooks wip

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -6,31 +6,40 @@ export default function Answer(props) {
     const {state} = useLocation();
     const { what, howMany } = state;
     const {ia, setIa} = useState([]);
-    let friendsArray = ['ala', 'ela', 'hela'];
-    const [friends, setFriends] = useState(friendsArray);
+    const [imagesArrayLength, setImagesArrayLength] = useState(howMany);
+    const [goodsName, setGoodsName] = useState('');
+    const [goodsValue, setGoodsValue] = useState(0);
+    const [rerender, setRerender] = useState(0);
 
     
-const handleAddFriend = () => {
-    setFriends((prevFriends) => [
-        ...prevFriends,
-        {
-            name: "Random Friend Name",
-            age: 20, // Random age
-        },
-    ]);
-};
+// const handleAddFriend = () => {
+//     setFriends((prevFriends) => [
+//         ...prevFriends,
+//         {
+//             name: "Random Friend Name",
+//             age: 20, // Random age
+//         },
+//     ]);
+// };
 
     useEffect(() => {
-        console.log('hello', howMany);
-        makeImageArray(howMany);
-        console.log('opopop', ia);
-    });
-    
-    const makeImageArray = (howMany) => {
-        let imageArray = [...Array(parseInt(howMany)).keys()]
-        console.log('dasda', imageArray);
+        
+            setGoodsName('vacine');
+            setGoodsValue(0.5);
+            console.log('floor', imagesArrayLength);
+            console.log('gval', goodsValue);
+             setImagesArrayLength(Math.floor(howMany / goodsValue));
+        
+        
+        console.log('imagesArrayLength', imagesArrayLength);
+    },[]);
 
-      }
+     function iaa(howMany) {
+        return howMany+2;
+    }
+
+    
+  
 
     return (
         <div>
@@ -38,19 +47,19 @@ const handleAddFriend = () => {
             <h2>From AskBalance</h2>
             <h3>{what}</h3>
             <h3>za {howMany}</h3>
-            <div>
             
-            <ul>
-                // Mapping over array of friends
-                {friends.map((friend, index) => (
-                    // Setting "index" as key because name and age can be repeated, It will be better if you assign uniqe id as key
-                    <li key={index}>
-                        <span>name: {friend.name}</span>{" "}
-                        <span>age: {friend.age}</span>
-                    </li>
-                ))}
-                <button onClick={handleAddFriend}>Add Friends</button>
-            </ul>
+            <div className="images-container">
+                {
+                        
+                    [...Array(iaa()).keys()].map(function(el, key) {
+                        return(
+                            
+                                <div className="image-cell" key={key}>{key}</div>
+                            
+                        )
+                    })
+                }
+            
             </div>
             <div>
                 <Link to="/balance_2/askbalance">to Ask Balance</Link>
